c898828d5d919b2462a1c4bbdddf37231b07e47c
Jahidul Islam <57562158+jahidulislam114593@users.noreply.github.com>
2024-09-14 12:41:22 +0600

    Time: 12 ms (34.51%), Space: 8.5 MB (37.86%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int n) {
+        if(n < 0) return 0;
+        int dup = n;
+        long long rev = 0;
+        while(n != 0)
+        {
+            int ld = n % 10;
+            n/=10;
+
+            rev = (rev*10) + ld;
+        }
+        if(dup == rev) return 1;
+        else return 0;
+    }
+};
