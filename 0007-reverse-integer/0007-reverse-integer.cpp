84354ca98ba6be3a6224ea71f54853912950aa26
Jahidul Islam <57562158+jahidulislam114593@users.noreply.github.com>
2024-09-14 12:10:30 +0600

    Time: 0 ms (100%), Space: 8.3 MB (81.17%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,6 +5,7 @@ public:
         while(x != 0)
         {
             int ld = x % 10;
+            //if revN * 10 > INT_MAX return 0 -> revN > INT_MAX / 10 -> same for INT_MIN as well
             if((revN > INT_MAX / 10) || (revN < INT_MIN / 10) ) return 0;
             revN = (revN * 10)+ ld;
             x /= 10;
