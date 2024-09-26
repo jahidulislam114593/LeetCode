82526facf08735e317b73418ade2ace85debdc2d
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-09-26 12:35:12 +0600

    Time: 4 ms (21.99%), Space: 8.1 MB (97.27%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+        //BruteForce Approch TC-> O(n)
+        // double ans = 1;
+        // if(n<0)
+        // {
+        //     // x = 2-> x= 1/2
+        //     //n = -2 -> n=-(-2) -> n = 2
+        //     x = 1 / x;
+        //     n = -n;
+        // }
+        // for(int i = 0; i<n; i++) ans *= x;
+
+    //Binary Exponentiation TC-> O(logn)
+        long binaryForm = n;
+        if(n<0)
+        {
+            x = 1 / x;
+            binaryForm = -binaryForm;
+        }
+
+        double ans = 1;
+        while(binaryForm != 0)
+        {
+            if(binaryForm % 2 == 1)
+            {
+                ans *= x;
+            }
+            x *= x;
+            binaryForm /= 2;
+        }
+
+        return ans;
+    }
+};
