e931c4f8385ed9372072c7152519ecf533acde52
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-10-09 13:44:44 +0600

    Time: 4 ms (62.14%), Space: 21.9 MB (88.39%) - LeetHub

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/3236-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/3236-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -3,25 +3,28 @@ public:
     int missingInteger(vector<int>& arr) {
         int sum = arr[0];
         int n = arr.size();
-
-        unordered_set<int>s;
-        for(auto el:arr) s.insert(el);
-
-        for(int i = 1; i<n; i++)
-        {
-            if(arr[i] == arr[i-1] + 1)
-            {
-                sum += arr[i];
-            }else
-            {
-                break;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i - 1] + 1) {
+                sum += arr[i]; 
+            } else {
+                break; 
             }
         }
 
-        while(s.find(sum) != s.end())
-        {
-            sum++;
+        int missing = sum;
+        bool found = true;
+
+        while (found) {
+            found = false;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == missing) {
+                    missing++; 
+                    found = true;
+                    break; 
+                }
+            }
         }
-        return sum;
+
+        return missing; 
     }
 };
