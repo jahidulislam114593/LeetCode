395af2d90a248a2a1111fa4ae8cc87a1417b68f2
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-10-20 20:57:35 +0600

    Time: 3 ms (99.13%), Space: 95.5 MB (34.98%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,19 +4,20 @@ public:
         int n1 = a.size(), n2 = b.size();
         int i = 0, j = 0;
         vector<int>c;
-        while(i<n1 && j<n2)
+        while(i < n1 && j < n2)
         {
             if(a[i] < b[j]) c.push_back(a[i++]);
             else c.push_back(b[j++]);
         }
-        while(i<n1) c.push_back(a[i++]);
-        while(j<n2) c.push_back(b[j++]);
+        while(i < n1) c.push_back(a[i++]);
+        while(j < n2) c.push_back(b[j++]);
 
         int n = n1 + n2;
         if(n & 1)
         {
             return c[n/2];
         }
+
         return double(c[n/2] + c[n/2 - 1]) / 2.0;
     }
 };
