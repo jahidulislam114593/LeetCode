07a2fe43a180d018802d892a954e61779186bd65
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-10-21 18:47:51 +0600

    Time: 2 ms (97.31%), Space: 49.6 MB (17.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -1,13 +1,12 @@
 /**
  * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @return {void} 
  */
 var sortColors = function(nums) {
     let n = nums.length;
     for (let i = 0; i < n - 1; i++) {
         for (let j = 0; j < n - i - 1; j++) {
             if (nums[j] > nums[j + 1]) {
-                // Swap nums[j] and nums[j+1]
                 let temp = nums[j];
                 nums[j] = nums[j + 1];
                 nums[j + 1] = temp;
