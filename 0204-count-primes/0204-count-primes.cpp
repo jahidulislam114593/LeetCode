f6e547519f091265ff482288dce8ba6291565b4a
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-11-11 20:49:08 +0600

    Time: 183 ms (83.56%), Space: 132 MB (14.05%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<int> isPrime(n, true);
+        int cnt = 0;
+        for(int i = 2; i<n; i++)
+        {
+            if(isPrime[i])
+            {
+                cnt++;
+                for(int j = i * 2; j<n; j = j+i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        return cnt;
+    }
+};
