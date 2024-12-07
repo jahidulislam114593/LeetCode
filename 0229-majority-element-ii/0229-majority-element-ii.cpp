3fdafe2088028ee7196d9b8a9340735822924cd4
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-12-07 21:50:33 +0600

    Time: 0 ms (100%), Space: 19.9 MB (28.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,18 +2,39 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        map<int,int>mpp;
-        vector<int>ls;
-        int mn = (n/3) + 1;
+        int freq1 = 0, freq2 = 0;
+        int ele1, ele2;
         for(int i = 0; i<n; ++i)
         {
-            mpp[nums[i]]++;
-            if(mpp[nums[i]] == mn)
+            if(freq1 == 0 && ele2 != nums[i])
             {
-                ls.push_back(nums[i]);
+                ele1 = nums[i];
+                freq1 = 1;
+            }else if(freq2 == 0 && ele1 != nums[i])
+            {
+                ele2 = nums[i];
+                freq2 = 1;
             }
-            if(ls.size() == 2) return ls;
+            else if(ele1 == nums[i]) freq1++;
+            else if(ele2 == nums[i]) freq2++;
+            else
+            {
+                freq1--;
+                freq2--;
+            }
         }
+
+        vector<int>ls;
+        freq1 = 0, freq2 = 0;
+        for(int i = 0; i<n; ++i)
+        {
+            if(ele1 == nums[i]) freq1++;
+            if(ele2 == nums[i]) freq2++;
+        }
+        int mn = (n/3) + 1;
+        if(freq1 >= mn) ls.push_back(ele1);
+        if(freq2 >= mn && ele1 != ele2) ls.push_back(ele2);
+
         return ls;
     }
 };
