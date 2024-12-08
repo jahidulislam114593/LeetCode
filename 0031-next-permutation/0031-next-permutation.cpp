f5ac7e05c0c5de7f56c3e3669deb648bd1dfefd2
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-12-08 10:41:46 +0600

    Time: 0 ms (100%), Space: 15.5 MB (15.83%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int idx = -1;
         int n = nums.size();
+        int idx = -1;
 
-        // 1. longest prefix / Peak 2 1(downfall) 5 4 3 0 0
         for(int i = n - 2; i >= 0; i--)
         {
             if(nums[i] < nums[i+1])
@@ -13,23 +12,17 @@ public:
                 break;
             }
         }
-        if(idx == -1)
-        {
-            reverse(nums.begin(), nums.end());
-            return;
-        }
 
-        // 2. find the closest value to nums[idx]
-        for(int i = n - 1; i > idx; i--)
+        if(idx == -1) return reverse(nums.begin(), nums.end());
+
+        for(int i = n - 1; i > idx; --i)
         {
-            if(nums[i] > nums[idx])
+            if(nums[idx] < nums[i])
             {
-                swap(nums[i], nums[idx]);
+                swap(nums[idx], nums[i]);
                 break;
             }
         }
-
-        //3. sort the remaining value
 
         reverse(nums.begin() + idx + 1, nums.end());
     }
