52a64a2e2b0352b9a1fc0baef911e3607474d6f2
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-12-11 13:33:57 +0600

    Time: 0 ms (100%), Space: 13.4 MB (8.36%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,10 +4,11 @@ public:
     bool searchInRow(vector<vector<int>>& mat, int target, int row)
     {
         int n = mat[0].size();
-        int st = 0, en = n - 1;
-        while(st <= en)
+        int st = 0, end = n - 1;
+
+        while(st <= end)
         {
-            int mid = st + (en - st) / 2;
+            int mid = st + (end - st) / 2;
             if(target == mat[row][mid])
             {
                 return true;
@@ -16,31 +17,33 @@ public:
                 st = mid + 1;
             }else 
             {
-                en = mid - 1;
+                end = mid - 1;
             }
         }
         return false;
     }
 
+
     bool searchMatrix(vector<vector<int>>& mat, int target) {
-        int m = mat.size(), n = mat[0].size();
-        int st = 0, en = m - 1;
-        while(st <= en)
+        int m = mat.size();
+        int n = mat[0].size();
+
+        int st = 0, end = m - 1;
+        while(st <= end)
         {
-            int midRow = st + (en - st) / 2;
-            if(target >= mat[midRow][0] && target <= mat[midRow][n-1])
+            int mid = st + (end - st) / 2;
+            if(target >= mat[mid][0] && target <= mat[mid][n-1])
             {
                 //BS
-                return searchInRow(mat, target, midRow);
-            }else if(target >= mat[midRow][n-1])
+                return searchInRow(mat, target, mid);
+            }else if(target >= mat[mid][n-1])
             {
-                st = midRow + 1;
-            }else
+                st = mid + 1;
+            }else 
             {
-                en = midRow - 1;
+                end = mid - 1;
             }
         }
-
-        return false;
+        return false; 
     }
 };
