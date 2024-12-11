5b64aa334864fa68ea3157fe90d0c156c6c12ee1
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-12-11 13:09:03 +0600

    Time: 0 ms (100%), Space: 13.4 MB (8.36%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,14 +1,46 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        for(int i = 0; i<n; ++i)
+
+    bool searchInRow(vector<vector<int>>& mat, int target, int row)
+    {
+        int n = mat[0].size();
+        int st = 0, en = n - 1;
+        while(st <= en)
         {
-            for(int j = 0; j<matrix[i].size(); ++j)
+            int mid = st + (en - st) / 2;
+            if(target == mat[row][mid])
             {
-                if(matrix[i][j] == target) return true;
+                return true;
+            }else if(target > mat[row][mid])
+            {
+                st = mid + 1;
+            }else 
+            {
+                en = mid - 1;
             }
         }
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int m = mat.size(), n = mat[0].size();
+        int st = 0, en = m - 1;
+        while(st <= en)
+        {
+            int midRow = st + (en - st) / 2;
+            if(target >= mat[midRow][0] && target <= mat[midRow][n-1])
+            {
+                //BS
+                return searchInRow(mat, target, midRow);
+            }else if(target >= mat[midRow][n-1])
+            {
+                st = midRow + 1;
+            }else
+            {
+                en = midRow - 1;
+            }
+        }
+
         return false;
     }
 };
