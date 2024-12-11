560bef8f9b941f6462efd362d3995d69a2bbfcc6
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-12-11 13:39:07 +0600

    Time: 0 ms (100%), Space: 13.4 MB (8.36%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
 
-    bool searchInRow(vector<vector<int>>& mat, int target, int row)
+    bool searchInRow(vector<vector<int>>& mat, int k, int row)
     {
         int n = mat[0].size();
         int st = 0, end = n - 1;
-
         while(st <= end)
         {
             int mid = st + (end - st) / 2;
-            if(target == mat[row][mid])
+            if(k == mat[row][mid]) return true;
+            else if(k > mat[row][mid]) st = mid + 1;
+            else end = mid - 1;
+        }
+        return false;
+    }
+
+
+    bool searchMatrix(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        int st = 0, end = m - 1;
+        while(st <= end)
+        {
+            int mid = st + (end - st) / 2;
+            if(k >= mat[mid][0] && k <= mat[mid][n-1])
             {
-                return true;
-            }else if(target > mat[row][mid])
+                // BS
+                return searchInRow(mat, k, mid);
+            }else if(k > mat[mid][n-1])
             {
                 st = mid + 1;
             }else 
@@ -21,29 +37,5 @@ public:
             }
         }
         return false;
-    }
-
-
-    bool searchMatrix(vector<vector<int>>& mat, int target) {
-        int m = mat.size();
-        int n = mat[0].size();
-
-        int st = 0, end = m - 1;
-        while(st <= end)
-        {
-            int mid = st + (end - st) / 2;
-            if(target >= mat[mid][0] && target <= mat[mid][n-1])
-            {
-                //BS
-                return searchInRow(mat, target, mid);
-            }else if(target >= mat[mid][n-1])
-            {
-                st = mid + 1;
-            }else 
-            {
-                end = mid - 1;
-            }
-        }
-        return false; 
     }
 };
