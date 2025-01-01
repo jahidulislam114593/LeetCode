26a07eb5e1479f5fb6f232e2efe71667b6ebac7b
Jahidul Islam <jahidul.islam114593@gmail.com>
2025-01-01 10:46:03 +0600

    Time: 52 ms (50.64%), Space: 31.2 MB (28.07%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,10 +4,10 @@ public:
         int n = nums.size();
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-
         for(int i = 0; i<n; ++i)
-        {
+        {   
             if(i > 0 && nums[i] == nums[i-1]) continue;
+
             int j = i + 1;
             int k = n - 1;
             while(j < k)
@@ -16,14 +16,14 @@ public:
                 if(sum < 0)
                 {
                     j++;
-                }else if(sum > 0)
+                }else if (sum > 0)
                 {
                     k--;
                 }else
                 {
                     vector<int>tmp = {nums[i], nums[j], nums[k]};
                     ans.push_back(tmp);
-                    j++, k--;
+                    j++,k--;
                     while(j < k && nums[j] == nums[j-1]) j++;
                     while(j < k && nums[k] == nums[k+1]) k--;
                 }
